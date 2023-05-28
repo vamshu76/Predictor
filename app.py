24729e1b9d3c01e86554de82b05a223f94b05f71
vamshu76 <131027002+vamshu76@users.noreply.github.com>
2023-05-28 12:09:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ st.set_page_config(
 iit_logo = "iitr_logo.jpg"
 image = open(iit_logo, "rb").read()
 b64_image = base64.b64encode(image).decode("utf-8")
-image_html = f'<img src="data:image/jpg;base64,{b64_image}" style="position: absolute; top: 50%; right: 0; transform: translate(-50%, -50%)">'
+image_html = f'<img src="data:image/jpg;base64,{b64_image}" style="position: fixed; top: 50%; right: 0; transform: translate(0, -50%);">'
 st.markdown(image_html, unsafe_allow_html=True)
 
 st.title('🧱 Micromechanical Properties Prediction Application')
