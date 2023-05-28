e8508a2fd43b0170e523f3e986aa0aab35e9fec9
vamshu76 <131027002+vamshu76@users.noreply.github.com>
2023-05-28 10:35:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -45,9 +45,9 @@ st.title('Micromechanical Properties Prediction Application')
 #pca_cs = pickle.load(open('pca_cs.pkl', 'rb'))
 #pca_ts = pickle.load(open('pca_ts.pkl', 'rb'))
 #pca_fs = pickle.load(open('pca_fs.pkl', 'rb'))
-#model_cs = pickle.load(open('cs.sav', 'rb'))
-#model_ts = pickle.load(open('ts.sav', 'rb'))
-#model_fs = pickle.load(open('fs.sav', 'rb'))
+model_cs = pickle.load(open('cs.sav', 'rb'))
+model_ts = pickle.load(open('ts.sav', 'rb'))
+model_fs = pickle.load(open('fs.sav', 'rb'))
 
 # inputs
 col1, col2, col3 = st.columns(3)
