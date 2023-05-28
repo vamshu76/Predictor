4814e49c67df0707ce3f34b7918ee13d29b8faa1
vamshu76 <131027002+vamshu76@users.noreply.github.com>
2023-05-28 12:48:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ st.set_page_config(
     layout="wide",
     initial_sidebar_state="expanded",
 )
-iit_logo = "logo_175.png"
+iit_logo = "iitr.jpg"
 image = open(iit_logo, "rb").read()
 b64_image = base64.b64encode(image).decode("utf-8")
 image_html = f'<img src="data:image/jpg;base64,{b64_image}" style="position: absolute; top: -40px; right: -40px; width: 450px; height: 130px;">'
